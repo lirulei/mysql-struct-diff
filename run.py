394721fdcb6c35b03277c63357cd6042575ccb60
Just9O <lirulei@users.noreply.github.com>
2023-03-05 18:10:14 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 # pip3 install mysql-connector-python==8.0.31
 import os
+import re
+import sys
+
 import mysql.connector
 import time
 import configs
@@ -7,6 +10,12 @@ import hashlib
 import difflib
 
 start_time = time.time()
+
+# 清除下历史文件
+try:
+    os.remove("compare.html")
+except Exception as e:
+    print(str(e))
 
 # 源端
 source_db = mysql.connector.connect(
@@ -62,18 +71,34 @@ print(f"src比dest多的表：", list(src_tb_set - dest_tb_set))
 
 # print("------------ 开始比较每个表的每个列信息 -------------------")
 
-base_sql = '''
-SELECT
- table_name,
-  column_name,
- CASE WHEN `COLUMN_DEFAULT` IS NULL THEN '' ELSE `COLUMN_DEFAULT` END AS `COLUMN_DEFAULT` ,
-  IS_NULLABLE,
-  COLUMN_TYPE,
-  COLUMN_KEY,
-  EXTRA
-FROM
-  information_schema.columns where table_schema = 
-'''
+if configs.check_charset_flag:
+    base_sql = '''
+    SELECT
+     table_name,
+      column_name,
+     CASE WHEN `COLUMN_DEFAULT` IS NULL THEN '' ELSE `COLUMN_DEFAULT` END AS `COLUMN_DEFAULT` ,
+      IS_NULLABLE,
+      COLUMN_TYPE,
+      CASE WHEN CHARACTER_SET_NAME IS NULL THEN '' ELSE CHARACTER_SET_NAME END AS CHARACTER_SET_NAME ,
+      CASE WHEN COLLATION_NAME IS NULL THEN '' ELSE COLLATION_NAME END AS COLLATION_NAME ,
+      COLUMN_KEY,
+      EXTRA
+    FROM
+      information_schema.columns where table_schema =
+    '''
+else:
+    base_sql = '''
+        SELECT
+         table_name,
+          column_name,
+         CASE WHEN `COLUMN_DEFAULT` IS NULL THEN '' ELSE `COLUMN_DEFAULT` END AS `COLUMN_DEFAULT` ,
+          IS_NULLABLE,
+          COLUMN_TYPE,
+          COLUMN_KEY,
+          EXTRA
+        FROM
+          information_schema.columns where table_schema = 
+        '''
 
 source_chksum = dict()
 dest_chksum = dict()
@@ -108,8 +133,9 @@ for i in dest_tb_set:
 
 # 5 进行集合运算
 print("-------------- 结果统计 ------------------")
+s1 = set()
 if source_chksum != dest_chksum:
-    s1 = set()
+
     differ = set(source_chksum.items()) ^ set(dest_chksum.items())
 
     for i in differ:
@@ -120,68 +146,75 @@ if source_chksum != dest_chksum:
         # 如果要全部都报出来，用下面这种写法
         s1.add(i[0])
 
-    print('表结构（只比对了列和索引，未包括字符集和排序集）检查完成，存在差异的表如下 ---> ', s1)
+    print('表结构检查完成，存在差异的表如下 ---> ', s1)
 else:
-    print('表结构（只比对了列和索引，未包括字符集和排序集）检查完成，没有发现存在差异的表')
+    print('表结构检查完成，没有发现存在差异的表')
 
 
 
-# 6 对上面采集到的有差异的表，分别在src和dest上执行 show create table 操作
-# 先创建个空文件，防止在结果集为空的情况下，导致后面的diff比较报错
-with open("src_tb_struct.txt", 'a') as f:
-    os.utime("src_tb_struct.txt", None)
-with open("dest_tb_struct.txt", 'a') as f:
-    os.utime("dest_tb_struct.txt", None)
+if len(s1) == 0:
+    print("未发现差异的表")
+else:
+    # 6 对上面采集到的有差异的表，分别在src和dest上执行 show create table 操作
+    # 先创建个空文件，防止在结果集为空的情况下，导致后面的diff比较报错
+    with open("src_tb_struct.txt", 'a') as f:
+        os.utime("src_tb_struct.txt", None)
+    with open("dest_tb_struct.txt", 'a') as f:
+        os.utime("dest_tb_struct.txt", None)
 
-for tb in s1:
-    show_create_tb_sql = "SHOW CREATE TABLE " + str(tb)
+    for tb in s1:
+        show_create_tb_sql = "SHOW CREATE TABLE " + str(tb)
+        try:
+            source_cursor.execute("use " + configs.mysql_source_db)
+            source_cursor.execute(show_create_tb_sql)
+            show_create_tb_result = source_cursor.fetchall()
+            # print(show_create_tb_result)
+            for ii in show_create_tb_result:
+                with open("src_tb_struct.txt", "a+") as f:
+                    # f.write(str(ii[1], ) + "\n")
+                    f.write(str(re.sub('AUTO_INCREMENT=\d+', '', str(ii[1])), ) + "\n")
+        except Exception as e:
+            print(str(e))
+            continue
+
+    for tb in s1:
+        show_create_tb_sql = "SHOW CREATE TABLE " + str(tb)
+        try:
+            dest_cursor.execute("use " + configs.mysql_dest_db)
+            dest_cursor.execute(show_create_tb_sql)
+            show_create_tb_result = dest_cursor.fetchall()
+            # print(show_create_tb_result)
+            for ii in show_create_tb_result:
+                with open("dest_tb_struct.txt", "a+") as f:
+                    # f.write(str( ii[1], ) + "\n")
+                    f.write(str(re.sub('AUTO_INCREMENT=\d+', '', str(ii[1])), ) + "\n")
+        except Exception as e:
+            print(str(e))
+            continue
+
+
+    # 7 调用 difflib ， 生成比对文件
+    src_result = open("src_tb_struct.txt", "r").readlines()
+    dest_result = open("dest_tb_struct.txt", "r").readlines()
+    difference = difflib.HtmlDiff(tabsize=2)
+    with open("compare.html", "w") as fp:
+        if sys.platform == 'linux':
+            html = difference.make_file(fromlines=src_result, tolines=dest_result, fromdesc="source", todesc="destination")
+        else:
+            html = difference.make_file(fromlines=src_result, tolines=dest_result, fromdesc="源端", todesc="目标端",charset='gbk')
+        fp.write(html)
+
+    # 8 删除临时文件
     try:
-        source_cursor.execute("use " + configs.mysql_source_db)
-        source_cursor.execute(show_create_tb_sql)
-        show_create_tb_result = source_cursor.fetchall()
-        # print(show_create_tb_result)
-        for ii in show_create_tb_result:
-            with open("src_tb_struct.txt", "a+") as f:
-                f.write(str(ii[1], ) + "\n")
+        os.remove("src_tb_struct.txt")
     except Exception as e:
         print(str(e))
-        continue
-
-for tb in s1:
-    show_create_tb_sql = "SHOW CREATE TABLE " + str(tb)
     try:
-        dest_cursor.execute("use " + configs.mysql_dest_db)
-        dest_cursor.execute(show_create_tb_sql)
-        show_create_tb_result = dest_cursor.fetchall()
-        # print(show_create_tb_result)
-        for ii in show_create_tb_result:
-            with open("dest_tb_struct.txt", "a+") as f:
-                f.write(str( ii[1], ) + "\n")
+        os.remove("dest_tb_struct.txt")
     except Exception as e:
         print(str(e))
-        continue
-
-
-# 7 调用 difflib ， 生成比对文件
-src_result = open("src_tb_struct.txt", "r").readlines()
-dest_result = open("dest_tb_struct.txt", "r").readlines()
-difference = difflib.HtmlDiff(tabsize=2)
-with open("compare.html", "w") as fp:
-    html = difference.make_file(fromlines=src_result, tolines=dest_result, fromdesc="source", todesc="destination")
-    fp.write(html)
-
-# 8 删除临时文件
-try:
-    os.remove("src_tb_struct.txt")
-except Exception as e:
-    print(str(e))
-try:
-    os.remove("dest_tb_struct.txt")
-except Exception as e:
-    print(str(e))
 
 # 9 统计下耗时
 stop_time = time.time()
 time_dur = stop_time - start_time
 print(f"耗时 {time_dur} 秒")
-
